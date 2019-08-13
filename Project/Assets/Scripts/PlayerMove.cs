626fc511049371c14f3630bd6f81ddc5b975924b
Nguyen Quoc Viet <nguyenqviet98@gmail.com>
2019-08-14 00:36:47 +0700

    add fps player controller

--- Project/Assets/Scripts/PlayerMove.cs
+++ Project/Assets/Scripts/PlayerMove.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private string horizontalInputName;
-    [SerializeField] private string verticalInputName;
+    [SerializeField] private string horizontalInputName = "Horizontal";
+    [SerializeField] private string verticalInputName = "Vertical";
+    [SerializeField] private float moveSpeed = 6f;
 
-    private float moveSpeed;
     private CharacterController characterController;
-
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+    private bool isJumping;
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
@@ -28,12 +31,42 @@ public class PlayerMove : MonoBehaviour
 
     void PlayerMovement()
     {
-        float vertInput = Input.GetAxis(verticalInputName) * moveSpeed;
-        float horizInput = Input.GetAxis(horizontalInputName) * moveSpeed;
+        float vertInput = Input.GetAxis(verticalInputName);
+        float horizInput = Input.GetAxis(horizontalInputName);
 
         Vector3 forwardMovement = transform.forward * vertInput;
         Vector3 rightMovement = transform.right * horizInput;
 
-        characterController.simpleMove(forwardMovement + rightMovement);
+        // clamp so diagonal movement can't be faster
+        characterController.SimpleMove(Vector3.ClampMagnatude(forwardMovement + rightMovement, 1) * moveSpeed);
+
+        JumpInput();
+    }
+
+    void JumpInput()
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    IEnumerator JumpEvent()
+    {
+        // for smoother jumping
+        characterController.slopeLimit = 90f;
+        float timeInAir = 0.0f;
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            characterController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!characterController.isGrounded && characterController.collisionFlags != CollisionFlags.Above);
+
+        // This will be done once the jumping finished
+        isJumping = false;
+        characterController.slopeLimit = 45f;
     }
 }
